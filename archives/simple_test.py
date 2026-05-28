82ef56da30b9c516b3dfcbdff2e220b2918e280a
saravanan v <saravananv1020@gmail.com>
2026-05-28 10:37:56 +0530

    Delete archives directory

--- archives/simple_test.py
+++
@@ -1,5 +0,0 @@
-# This is a test file
-print('Hello')
-
-def add(a, b):
-    return a + b
--- archives/test_interactive.py
+++
@@ -1,2 +0,0 @@
-# Test the interactive mode briefly with a simple command
-echo '{"task": "Create a function that calculates factorial of a number"}' > /tmp/test_task.json
